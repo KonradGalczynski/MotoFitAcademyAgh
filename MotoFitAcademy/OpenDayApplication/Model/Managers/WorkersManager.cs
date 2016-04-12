8bfa736373831339d7735d7c737da1b0ad8fc9ce
Gabriela Gozdziewicz - 3 <MotorolaScrumWorshop2@gmail.com>
2016-04-12 12:30:39 +0200

    Rozpoczęcie pracy nad walidają Imię i Nazwisko

--- MotoFitAcademy/OpenDayApplication/Model/Managers/WorkersManager.cs
+++ MotoFitAcademy/OpenDayApplication/Model/Managers/WorkersManager.cs
@@ -23,6 +23,18 @@ namespace OpenDayApplication.Model.Managers
     }
     public void AddWorker(Worker worker)
     {
+      /*  if (string.IsNullOrWhiteSpace(worker.Name) == true)
+        {
+            MessageBox.Show("Pole imię nie może być puste.");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(worker.Surname) == true)
+        {
+            MessageBox.Show("Pole nazwisko nie może być puste.");
+            return;
+        } */
+
       using (var dataContext = new MotoFitAcademyDataContext(Confiuration.GetSqlConnectionString()))
       {
         dataContext.Workers.InsertOnSubmit(worker);
